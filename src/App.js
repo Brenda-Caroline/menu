cee17a3218791c5ca6d5233910015b71fd030a89
Brenda-Caroline <71780774+Brenda-Caroline@users.noreply.github.com>
2020-11-19 15:59:58 -0300

    19/11

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <table>
+         <tr> <td><img src="https://pt.freelogodesign.org/Content/img/logo-samples/flooop.png" alt="Logo" /></td>
+        
+         
+
+        <td> <a href=""> <button className="btn"> Home</button></a></td>
+        <td> <a href=""> <button className="btn"> História da Associação</button></a></td>
+        <td> <a href=""> <button className="btn"> Rota do Queijo</button></a></td>
+        <td> <a href=""> <button className="btn"> Produtores</button></a></td>
+        <td> <a href=""> <button className="btn"> Lista de Desejos</button></a></td>
+        <td> <a href=""> <button className="btn"> Cadastrar Produtos</button></a></td>
+        <td> <a href=""> <button className="btn"> Login</button></a></td>
+        <td> <a href=""> <button className="btn"> Sair</button></a></td>
+        
+
+        <td> <a href="https://pt-br.facebook.com/queijoartesanaldaserrageral">
+        <img src="https://i.pinimg.com/originals/57/98/53/5798533bb3f2a08ee3514f9c6f6e03be.png" alt="Facebook" className="redes" title="Facebook"/>
+        </a></td>
+
+        <td> <a href="https://www.instagram.com/queijoartesanaldaserrageral?igshid=1qr782a69ojt0">
+        <img src="https://mairacuryteam.com.br/wp-content/uploads/2019/05/logo-instagram-png-fundo-transparente13-1-1024x1024.png" alt="Instagram"  className="redesInsta" title="Instagram"/>
+        </a></td>
+         </tr>
+              
+
+       </table>    
       </header>
     </div>
   );
